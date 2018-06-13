8135b4ac90ceb7c4470d91476a9b4e2facf749c9
toni <ferreirocouto@gmail.com>
2018-06-13 20:14:19 +0200

    Vista inicial con H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/*populate tables*/
+INSERT INTO cliente (id,nombre,apellido,email,create_at) VALUES (1,'Toni','Gonzalez Lopez','email@email.com','2018-02-20');
+INSERT INTO cliente (id,nombre,apellido,email,create_at) VALUES (2,'Sofia','Rodriguez Perez','email2@email.com','2018-06-01');
